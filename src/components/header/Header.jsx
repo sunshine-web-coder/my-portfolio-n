6489ee666ce28b5624f484abe6e1ea637af8de80
sunshine-web-coder <webcoderdev02@gmail.com>
2023-10-06 17:52:51 +0100

    cv file added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
+import cvFile from "../../file/Ezekiel-Fajuyagbe.pdf"
 
 export default function Header() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -33,7 +34,7 @@ export default function Header() {
                 <a href="#projects">WORKS</a>
               </li>
               <li>
-                <Link to="">VIEW RESUME</Link>
+                <Link to={cvFile} target="_blank">VIEW RESUME</Link>
               </li>
               <li>
                 <a href="#contact">HIRE ME</a>
